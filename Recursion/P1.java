594e8b0823908aa7b92a3f26d2a1d1b875a10273
BarnaliBhowmik <143362520+BarnaliBhowmik@users.noreply.github.com>
2025-07-14 06:15:35 +0530

    commit

---
+++ Recursion/P1.java
@@ -0,0 +1,28 @@
+// Print numbers form n to 1 (Decreasing Order)
+
+public class P1 {
+    public static void printDec(int n) {
+        if (n==1) {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n + " ");
+        printDec(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        printDec(n);
+    }
+}
+
+/* Call Stack : Decreasing Order
+public static void printDec(int n) {
+    if(n == 1) {
+        System.out.println(n);
+        return;
+    }
+    System.out.println(n);
+    printDec(n-1);
+}
+ */
